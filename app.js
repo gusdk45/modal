dd0539f928e0e97a907acdb02df2cafea79cf5ce
gusdk45 <gusdk1418@naver.com>
2024-02-14 17:13:06 +0900

    6주차 modal 과제

---
+++ app.js
@@ -0,0 +1,4 @@
+// modal-btn, modal-overlay, close-btn 선택합니다
+// modal-btn 및 close-btn에서 클릭 이벤트를 수신합니다
+// 사용자가 modal-btn을 클릭하면 modal- overlay에 .open- modal를 추가합니다
+// 사용자가 close-btn을 클릭하면 modal- overlay에서 .open- modal를 제거합니다
